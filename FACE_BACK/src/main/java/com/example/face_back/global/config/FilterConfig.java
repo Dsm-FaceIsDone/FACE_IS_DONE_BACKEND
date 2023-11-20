b9bac045e2a5a408425a1f21a68cbd4d3d229253
lee na kyung <ni1722650@gmail.com>
2023-11-21 08:33:27 +0900

    📑 :: Config

---
+++ FACE_BACK/src/main/java/com/example/face_back/global/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.example.face_back.global.config;
+
+import com.example.face_back.global.config.security.jwt.JwtTokenFilter;
+import com.example.face_back.global.config.security.jwt.JwtTokenProvider;
+import com.example.face_back.global.error.GlobalExceptionFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}

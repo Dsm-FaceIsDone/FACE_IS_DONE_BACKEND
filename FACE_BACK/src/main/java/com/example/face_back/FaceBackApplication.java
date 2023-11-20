dd50b03a2d49cc2281796148317bc9b4aec1cae8
lee na kyung <ni1722650@gmail.com>
2023-11-20 16:24:07 +0900

    ⚙️ :: SETTING

---
+++ FACE_BACK/src/main/java/com/example/face_back/FaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.face_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceBackApplication.class, args);
+    }
+
+}

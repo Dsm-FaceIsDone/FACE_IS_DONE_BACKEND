f058fed0a080c467dbc73dcfa28501afc81133a2
lee na kyung <ni1722650@gmail.com>
2023-11-20 17:12:45 +0900

    📑 :: SkeinRepository

---
+++ FACE_BACK/src/main/java/com/example/face_back/domain/skein/domain/repository/SkeinRepository.java
@@ -0,0 +1,7 @@
+package com.example.face_back.domain.skein.domain.repository;
+
+import com.example.face_back.domain.skein.domain.Skein;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SkeinRepository extends CrudRepository<Skein, Long> {
+}

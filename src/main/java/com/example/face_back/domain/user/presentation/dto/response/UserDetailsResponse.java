91e5c183b66091d1ccea2909ec269ac3e0433d3c
lee na kyung <ni1722650@gmail.com>
2023-12-01 08:15:54 +0900

    📑 :: AccountId -> UserId

--- src/main/java/com/example/face_back/domain/user/presentation/dto/response/UserDetailsResponse.java
+++ src/main/java/com/example/face_back/domain/user/presentation/dto/response/UserDetailsResponse.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Builder
 public class UserDetailsResponse {
     private final String nickname;
-    private final String accountId;
+    private final String userId;
     private final String profileImgUrl;
     private final long totalPosts;
 }

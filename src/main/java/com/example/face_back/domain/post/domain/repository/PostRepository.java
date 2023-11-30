2c53f6cf340d6fc97db5ad582350cf47192f5882
lee na kyung <ni1722650@gmail.com>
2023-12-01 08:12:45 +0900

    ⚡️ :: PostRepository

--- src/main/java/com/example/face_back/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/example/face_back/domain/post/domain/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package com.example.face_back.domain.post.domain.repository;
 
 import com.example.face_back.domain.post.domain.Post;
+import com.example.face_back.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-    
+    long countByUser(User user);
 }

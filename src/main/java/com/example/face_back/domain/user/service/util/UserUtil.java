ff4d891c2f58244e837e021379c88dff236473e3
lee na kyung <ni1722650@gmail.com>
2023-11-30 19:51:03 +0900

    📑 :: UserUtil Upload method

--- src/main/java/com/example/face_back/domain/user/service/util/UserUtil.java
+++ src/main/java/com/example/face_back/domain/user/service/util/UserUtil.java
@@ -4,15 +4,18 @@ import com.example.face_back.domain.user.domain.User;
 import com.example.face_back.domain.user.domain.repository.UserRepository;
 import com.example.face_back.domain.user.service.exception.UserNotFoundException;
 import com.example.face_back.global.config.security.jwt.exception.TokenUnauthorizedException;
+import com.example.face_back.infra.aws.service.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @Component
 @RequiredArgsConstructor
 public class UserUtil {
     private final UserRepository userRepository;
+    private final S3Util s3Util;
 
     public String getUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -22,5 +25,13 @@ public class UserUtil {
 
     public User getUser() {
         return userRepository.findById(getUserId()).orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public void upload(String userId, MultipartFile file) {
+        if (file != null && !file.isEmpty()) {
+            User user = userRepository.findByUserId(userId)
+                    .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+            user.updatePath(s3Util.upload(file));
+        }
     }
 }

2e55ab3ff9888b4940ff8d04defee62fa798c4ef
lee na kyung <ni1722650@gmail.com>
2023-11-30 19:07:43 +0900

    📑 :: RefreshToken

---
+++ src/main/java/com/example/face_back/domain/user/domain/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.example.face_back.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class RefreshToken {
+
+    @Id
+    private String userId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Builder
+    public RefreshToken (String accountId, String refreshToken) {
+        this.userId = accountId;
+        this.refreshToken = refreshToken;
+    }
+}

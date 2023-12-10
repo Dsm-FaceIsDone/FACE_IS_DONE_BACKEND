ac7f1fa6116cb7f9e7ba97ff08feea6378aa512b
lee na kyung <ni1722650@gmail.com>
2023-12-10 23:02:44 +0900

    📑 :: PasswordMisMatchException

---
+++ src/main/java/com/example/face_back/domain/user/service/exception/PasswordMisMatchException.java
@@ -0,0 +1,12 @@
+package com.example.face_back.domain.user.service.exception;
+
+import com.example.face_back.global.error.exception.BaseException;
+import com.example.face_back.global.error.exception.ErrorCode;
+
+public class PasswordMisMatchException extends BaseException {
+    public static final BaseException EXCEPTION = new PasswordMisMatchException();
+
+    private PasswordMisMatchException(){
+        super(ErrorCode.PASSWORD_MISMATCH);
+    }
+}

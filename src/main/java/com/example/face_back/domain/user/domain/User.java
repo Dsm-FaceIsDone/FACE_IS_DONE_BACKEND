14f5356a901ed59b3bbc73964ef3af210134b662
lee na kyung <ni1722650@gmail.com>
2023-12-15 18:56:13 +0900

    🗑 ::

--- src/main/java/com/example/face_back/domain/user/domain/User.java
+++ src/main/java/com/example/face_back/domain/user/domain/User.java
@@ -30,7 +30,6 @@ public class User {
         this.userId = userId;
         this.password = password;
         this.nickname = nickname;
-        this.path = "";
     }
 
     public String updatePath(String path) {

2abe02b9a282d7d154e766c682942aad9fd397ce
lee na kyung <ni1722650@gmail.com>
2023-12-28 10:01:06 +0900

    📑 :: SkeinRepository

--- src/main/java/com/example/face_back/domain/skein/domain/repository/SkeinRepository.java
+++ src/main/java/com/example/face_back/domain/skein/domain/repository/SkeinRepository.java
@@ -1,7 +1,11 @@
 package com.example.face_back.domain.skein.domain.repository;
 
+import com.example.face_back.domain.post.domain.Post;
 import com.example.face_back.domain.skein.domain.Skein;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SkeinRepository extends CrudRepository<Skein, Long> {
+    Optional<Skein> findAllByPostOrderByIdDesc(Post post);
 }

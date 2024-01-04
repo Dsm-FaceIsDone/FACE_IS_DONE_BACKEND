818385cdfbd9a2934a5a1c72c9cd8e60a831b421
lee na kyung <ni1722650@gmail.com>
2024-01-04 23:15:26 +0900

    ⚡️ :: PostListResponse - static declaration, add profile image

--- src/main/java/com/example/face_back/domain/post/presentation/dto/response/PostListResponse.java
+++ src/main/java/com/example/face_back/domain/post/presentation/dto/response/PostListResponse.java
@@ -1,5 +1,6 @@
 package com.example.face_back.domain.post.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,17 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public class PostListResponse {
+    private final int totalPosts;
     private final List<PostResponse> postResponses;
 
+    @Getter
     @Builder
-    public class PostResponse {
+    public static class PostResponse {
         private final Long id;
         private final String userId;
         private final String nickname;
         private final String content;
-        private final String file;
+        private final String profileImage;
         private final Integer heartCount;
     }
 }

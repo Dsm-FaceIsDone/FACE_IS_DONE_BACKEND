f15f8e6ec2f278feea40289e099f8d166b248bbd
velyn666 <ni1722650@gmail.com>
2024-01-04 23:11:56 +0900

    📑 :: PostListService

---
+++ src/main/java/com/example/face_back/domain/post/service/PostListService.java
@@ -0,0 +1,48 @@
+package com.example.face_back.domain.post.service;
+
+import com.example.face_back.domain.post.domain.Post;
+import com.example.face_back.domain.post.domain.repository.PostRepository;
+import com.example.face_back.domain.post.presentation.dto.response.PostListResponse;
+import com.example.face_back.domain.user.service.util.UserUtil;
+import com.example.face_back.infra.aws.service.S3Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostListService {
+    private final PostRepository postRepository;
+    private final UserUtil userUtil;
+    private final S3Util s3Util;
+
+    public PostListResponse getUserPostsPaged(Pageable pageable) {
+        Page<Post> posts = postRepository.findAllByUserOrderByIdDesc(userUtil.getUser(), pageable);
+
+        return new PostListResponse(posts.getTotalPages(),
+                posts.stream().map(this::ofPostResponse).collect(Collectors.toList()));
+    }
+
+    public PostListResponse findPost(String title, Pageable page) {
+        Page<Post> posts;
+
+        posts = postRepository.findAllByContentContainingOrderByIdDesc(title, page);;
+
+
+        return new PostListResponse(posts.getTotalPages(),
+                posts.stream().map(this::ofPostResponse).collect(Collectors.toList()));
+    }
+
+    private PostListResponse.PostResponse ofPostResponse(Post post) {
+        return PostListResponse.PostResponse.builder()
+                .id(post.getId())
+                .userId(post.getUser().getUserId())
+                .content(post.getContent())
+                .nickname(post.getUser().getNickname())
+                .profileImage(s3Util.getProfileImgUrl(post.getUser().getPath()))
+                .build();
+    }
+}

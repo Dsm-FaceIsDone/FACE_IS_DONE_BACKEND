391e3f369556b25ba9a076656028c90859e29d86
velyn666 <ni1722650@gmail.com>
2024-01-04 23:09:43 +0900

    📑 :: PostDetailsService

---
+++ src/main/java/com/example/face_back/domain/post/service/PostDetailsService.java
@@ -0,0 +1,43 @@
+package com.example.face_back.domain.post.service;
+
+import com.example.face_back.domain.post.domain.Post;
+import com.example.face_back.domain.post.domain.repository.PostRepository;
+import com.example.face_back.domain.post.presentation.dto.response.PostResponse;
+import com.example.face_back.domain.post.service.exception.PostNotFoundException;
+import com.example.face_back.domain.skein.presentation.dto.response.SkeinResponse;
+import com.example.face_back.infra.aws.service.S3Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class PostDetailsService {
+    private final PostRepository postRepository;
+    private final S3Util s3Util;
+
+    @Transactional(readOnly = true)
+    public PostResponse getPostDetails(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(()-> PostNotFoundException.EXCEPTION);
+
+        return PostResponse.builder()
+                .id(post.getId())
+                .userId(post.getUser().getUserId())
+                .nickname(post.getUser().getNickname())
+                .profileImage(s3Util.getProfileImgUrl(post.getUser().getPath()))
+                .content(post.getContent())
+                .file(s3Util.getPostImgUrl(post.getPath()))
+                .skeins(post.getSkeins().stream().map(skein -> {
+                    return SkeinResponse.builder()
+                            .id(skein.getId())
+                            .userNickname(skein.getUser().getNickname())
+                            .profileImage(s3Util.getProfileImgUrl(skein.getUser().getPath()))
+                            .build();
+                    }
+                ).collect(Collectors.toList()))
+                .build();
+    }
+}

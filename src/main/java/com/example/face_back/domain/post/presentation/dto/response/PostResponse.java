abf8ec80ba88be680072731db6c92345eeaff2a2
lee na kyung <ni1722650@gmail.com>
2024-01-04 23:13:39 +0900

    ⚡️ :: PostResponse - add profile image, skeins list

--- src/main/java/com/example/face_back/domain/post/presentation/dto/response/PostResponse.java
+++ src/main/java/com/example/face_back/domain/post/presentation/dto/response/PostResponse.java
@@ -1,7 +1,10 @@
 package com.example.face_back.domain.post.presentation.dto.response;
 
+import com.example.face_back.domain.skein.presentation.dto.response.SkeinResponse;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class PostResponse {
@@ -9,16 +12,20 @@ public class PostResponse {
     private final String userId;
     private final String nickname;
     private final String content;
+    private final String profileImage;
     private final String file;
+    private final List<SkeinResponse> skeins;
     private final Integer heartCount;
 
     @Builder
-    private PostResponse(Long id, String userId, String nickname, String content, String file, Integer heartCount){
+    private PostResponse(Long id, String userId, String nickname, String content, String file, List<SkeinResponse> skeins, Integer heartCount, String profileImage){
         this.id = id;
         this.userId = userId;
         this.nickname = nickname;
         this.content = content;
+        this.profileImage = profileImage;
         this.file = file;
+        this.skeins = skeins;
         this.heartCount = heartCount;
     }
 }

9e593d945be727d73fe7e1d54330e758f00b30cb
lee na kyung <ni1722650@gmail.com>
2024-01-04 23:12:29 +0900

    ⚡️ :: PostRepository - add find content, user

--- src/main/java/com/example/face_back/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/example/face_back/domain/post/domain/repository/PostRepository.java
@@ -2,8 +2,12 @@ package com.example.face_back.domain.post.domain.repository;
 
 import com.example.face_back.domain.post.domain.Post;
 import com.example.face_back.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+    Page<Post> findAllByContentContainingOrderByIdDesc(String content, Pageable pageable);
+    Page<Post> findAllByUserOrderByIdDesc(User user, Pageable pageable);
     long countByUser(User user);
 }

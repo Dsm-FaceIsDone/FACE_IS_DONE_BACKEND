a25e00e2b8838ff7330fe3e620e9fe79bbcac2ec
lee na kyung <ni1722650@gmail.com>
2024-01-04 23:17:13 +0900

    📑 :: PostController

--- src/main/java/com/example/face_back/domain/post/presentation/PostController.java
+++ src/main/java/com/example/face_back/domain/post/presentation/PostController.java
@@ -1,4 +1,58 @@
 package com.example.face_back.domain.post.presentation;
 
+import com.example.face_back.domain.post.presentation.dto.request.PostRequest;
+import com.example.face_back.domain.post.presentation.dto.response.PostListResponse;
+import com.example.face_back.domain.post.presentation.dto.response.PostResponse;
+import com.example.face_back.domain.post.presentation.dto.response.ReturnIdResponse;
+import com.example.face_back.domain.post.service.PostDetailsService;
+import com.example.face_back.domain.post.service.PostListService;
+import com.example.face_back.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+    private final PostListService postListService;
+    private final PostDetailsService postDetailsService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReturnIdResponse create (@RequestBody @Valid PostRequest request) {
+        return postService.create(request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull Long id) {
+        postService.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse getPostDetails(@PathVariable @NotNull Long id) {
+        return postDetailsService.getPostDetails(id);
+    }
+
+    @PostMapping(value = "/postImage/{id}", consumes = "multipart/form-data")
+    public void postImage(@PathVariable @NotNull Long id, @RequestPart(value = "image") MultipartFile file) {
+        postService.postImage(id, file);
+    }
+
+    @GetMapping("/user")
+    public PostListResponse getUserPostsPaged(Pageable pageable) {
+        return postListService.getUserPostsPaged(pageable);
+    }
+
+    @GetMapping("/search")
+    public PostListResponse findPost(@RequestParam(value = "content") String content, Pageable page) {
+        return postListService.findPost(content, page);
+    }
 }

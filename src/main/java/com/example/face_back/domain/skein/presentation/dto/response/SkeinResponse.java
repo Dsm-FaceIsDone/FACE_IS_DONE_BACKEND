c6e9b62d87e67609b90a0b013d339b28c8c74fd7
velyn666 <ni1722650@gmail.com>
2024-01-05 16:58:41 +0900

    ⚡️ :: SkeinResponse - add profile image, file

--- src/main/java/com/example/face_back/domain/skein/presentation/dto/response/SkeinResponse.java
+++ src/main/java/com/example/face_back/domain/skein/presentation/dto/response/SkeinResponse.java
@@ -8,11 +8,15 @@ public class SkeinResponse {
     private final Long id;
     private final String userNickname;
     private final String userId;
+    private final String file;
+    private final String profileImage;
 
     @Builder
-    public SkeinResponse(Long id, String userNickname, String userId){
+    public SkeinResponse(Long id, String userNickname, String userId, String file, String profileImage){
         this.id = id;
         this.userNickname = userNickname;
         this.userId = userId;
+        this.file = file;
+        this.profileImage = profileImage;
     }
 }

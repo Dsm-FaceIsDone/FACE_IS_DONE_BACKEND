190bec0a46feaced0bfa602fcb6e1217c45ffba0
velyn666 <ni1722650@gmail.com>
2024-01-07 00:08:31 +0900

    ⚡️ :: HeartRepository

--- src/main/java/com/example/face_back/domain/heart/domain/repository/HeartRepository.java
+++ src/main/java/com/example/face_back/domain/heart/domain/repository/HeartRepository.java
@@ -1,7 +1,12 @@
 package com.example.face_back.domain.heart.domain.repository;
 
 import com.example.face_back.domain.heart.domain.Heart;
+import com.example.face_back.domain.post.domain.Post;
+import com.example.face_back.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+    Boolean existsByUserAndPost(User user, Post post);
+
+    void deleteByUserAndPost(User user, Post post);
 }

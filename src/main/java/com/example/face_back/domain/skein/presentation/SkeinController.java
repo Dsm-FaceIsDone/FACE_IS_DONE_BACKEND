8b27d843570cd06db2c5273d465e1c83280a8f5b
velyn666 <ni1722650@gmail.com>
2024-01-08 14:25:48 +0900

    📑 :: SkeinController

--- src/main/java/com/example/face_back/domain/skein/presentation/SkeinController.java
+++ src/main/java/com/example/face_back/domain/skein/presentation/SkeinController.java
@@ -1,4 +1,35 @@
 package com.example.face_back.domain.skein.presentation;
 
+import com.example.face_back.domain.post.presentation.dto.response.ReturnIdResponse;
+import com.example.face_back.domain.skein.presentation.dto.request.SkeinRequest;
+import com.example.face_back.domain.skein.service.SkeinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@RequestMapping("/skein")
+@RestController
+@RequiredArgsConstructor
 public class SkeinController {
+    private final SkeinService skeinService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReturnIdResponse create(@RequestBody @Valid SkeinRequest request) {
+        return skeinService.creat(request);
+    }
+    @PostMapping(value = "/skeinImage/{id}", consumes = "multipart/form-data")
+    public void skeinImage(@PathVariable @NotNull Long id, @RequestPart(value = "image") MultipartFile file) {
+        skeinService.skeinImage(id, file);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull Long id) {
+        skeinService.delete(id);
+    }
 }
